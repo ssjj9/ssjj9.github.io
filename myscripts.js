1fee9dc22618d78dfac23a3178c2a300735f31a6
ssjj9 <narehtse9@gmail.com>
2017-03-04 14:01:17 -0800

    Add TicTacToe code

--- myscripts.js
+++ myscripts.js
@@ -43,3 +43,17 @@ function goRight(){
     document.getElementById("pic").src=pics[picIndex];
   }
 }
+
+var character = "";
+
+function chooseX(){
+	character = 'X';
+}
+
+function chooseO(){
+	character = 'O';
+}
+
+function makeMove(divLocation){
+	document.getElementById(divLocation).innerHTML=character;
+}
